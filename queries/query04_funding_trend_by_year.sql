a472182eaf226a66151cffbe9f94a92f1d4325d9
nidhimanghnani <nidhi.manghnani@gmail.com>
2026-07-11 22:03:50 +0530

    Delete queries directory

--- queries/query04_funding_trend_by_year.sql
+++
@@ -1,7 +0,0 @@
-SELECT EXTRACT(YEAR FROM SAFE.PARSE_DATE('%d/%m/%Y', Date)) AS year,
-COUNT(*) AS total_deals
-FROM `startup_funding.funding`
-WHERE Date IS NOT NULL
-AND Date != 'Date dd/mm/yyyy'
-GROUP BY year
-ORDER BY year
